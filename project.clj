c179560a92717c7df434fc8a9672b595a8b594e8
karneaud <kendall.arneaud@gmail.com>
2016-07-26 19:31:59 -0400

    fix repl/ ring standalone settings

--- project.clj
+++ project.clj
@@ -30,13 +30,12 @@
                           :output-to "resources/public/js/main.js"
                           :output-dir "resources/public/js/out"
                           :asset-path "js/out"
-
                           ;;:pretty-print true
                           ;;:optimizations :none
 
                           }}]}
   :aliases {
-    "start-dev" ["pdo" ["cljsbuild" "auto"] ["run" "-m" "app2.server/app"]]
+    "start-dev" ["pdo" ["cljsbuild" "auto"] ["ring" "server-headless"]]
   }
   :figwheel {
     :css-dirs ["resources/public/css"]
@@ -48,6 +47,17 @@
   :source-paths ["src"]
   :resource-paths ["resources"]
   :main app2.server
+  :repl-options {
+                :prompt (fn [ns] (str "your command for <" ns ">, master? " ))
+                :welcome (println "Welcome to the magical world of the repl!")
+                :init-ns app2.hawk
+                :init (app2.hawk/init)
+                :caught clj-stacktrace.repl/pst+
+                :skip-default-init false
+                :host "0.0.0.0"
+                :port 9000
+                :timeout 40000
+              }
   :ring {
     :handler app2.server/app
     :auto-reload? true :auto-refresh? true :reload-paths ["resources/public"]
@@ -56,11 +66,11 @@
     :dev {
           :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
           :source-paths ["cljs_src"]
-
             :ring {
               :nrepl {
                 :start? true
                 :port 9000
+                :host "0.0.0.0"
               }
             }
             :dependencies [
@@ -71,53 +81,13 @@
               [figwheel-sidecar "0.5.4-7"]
               [org.clojure/tools.nrepl "0.2.11"]
               [com.cemerick/piggieback "0.2.1"]
+              [hawk "0.2.10"]
             ]
             :plugins [
               [lein-pdo "0.1.1"]
               [lein-figwheel "0.5.4-7"]
             ]
           }
-   }
-   :repl-options {
-                 :prompt (fn [ns] (str "your command for <" ns ">, master? " ))
-                 ;; What to print when the repl session starts.
-                 :welcome (println "Welcome to the magical world of the repl!")
-                 ;; Specify the ns to start the REPL in (overrides :main in
-                 ;; this case only)
-                 ;;:init-ns foo.bar
-                 ;; This expression will run when first opening a REPL, in the
-                 ;; namespace from :init-ns or :main if specified.
-                 :init (require '[app2.figwheel])
-                 ;; Print stack traces on exceptions (highly recommended, but
-                 ;; currently overwrites *1, *2, etc).
-                 :caught clj-stacktrace.repl/pst+
-                 ;; Skip's the default requires and printed help message.
-                 :skip-default-init false
-                 ;; Customize the socket the repl task listens on and
-                 ;; attaches to.
-                 :host "0.0.0.0"
-                 :port 9000
-                 ;; If nREPL takes too long to load it may timeout,
-                 ;; increase this to wait longer before timing out.
-                 ;; Defaults to 30000 (30 seconds)
-                 :timeout 40000
-                 ;;:verbose true
-                 ;; nREPL server customization
-                 ;; Only one of #{:nrepl-handler :nrepl-middleware}
-                 ;; may be used at a time.
-                 ;; Use a different server-side nREPL handler.
-                ;; :nrepl-handler (clojure.tools.nrepl.server/default-handler)
-                 ;; Add server-side middleware to nREPL stack.
-                ;  :nrepl-middleware [my.nrepl.thing/wrap-amazingness
-                ;                     ;; TODO: link to more detailed documentation.
-                ;                     ;; Middleware without appropriate metadata
-                ;                     ;; (see clojure.tools.nrepl.middleware/set-descriptor!
-                ;                     ;; for details) will simply be appended to the stack
-                ;                     ;; of middleware (rather than ordered based on its
-                ;                     ;; expectations and requirements).
-                ;                     (fn [handler]
-                ;                       (fn [& args]
-                ;                         (prn :middle args)
-                ;                         (apply handler args)))]
-                                      }
+        }
+
   )
